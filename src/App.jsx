c30dfe980c7225d557c7d18763a865f2833e0649
vuanhtuan123 <vuanhtuanzxc12@gmail.com>
2023-11-26 01:06:43 +0700

    làm dark-mode-system

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,76 @@ import ThreeDRotation from '@mui/icons-material/ThreeDRotation'
 import HomeIcon from '@mui/icons-material/Home'
 import { pink } from '@mui/material/colors'
 import Typography from '@mui/material/Typography'
+import { useColorScheme } from '@mui/material/styles'
+
+import InputLabel from '@mui/material/InputLabel'
+import MenuItem from '@mui/material/MenuItem'
+import FormControl from '@mui/material/FormControl'
+import Select from '@mui/material/Select'
+import Box from '@mui/system/Box'
+import LightModeIcon from '@mui/icons-material/LightMode'
+import DarkModeIcon from '@mui/icons-material/DarkModeOutlined'
+import SettingsBrightnessIcon from '@mui/icons-material/SettingsBrightness'
+function ModeSelect() {
+  const { mode, setMode } = useColorScheme()
+
+  const handleChange = (event) => {
+    const selectedMode = event.target.value
+    setMode(selectedMode)
+  }
+
+  return (
+    <>
+      <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+        <InputLabel id="label-select-dark-light-mode">Mode</InputLabel>
+        <Select
+          labelId="label-select-dark-light-mode"
+          id="select-dark-light-mode"
+          value={mode}
+          label="Mode"
+          onChange={handleChange}
+        >
+          <MenuItem value="light">
+            <div style={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
+              <LightModeIcon fontSize='small'/>Light
+            </div>
+          </MenuItem>
+          <MenuItem value="dark">
+            <Box sx={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
+              <DarkModeIcon fontSize='small'/>Dark
+            </Box>
+          </MenuItem>
+          <MenuItem value="system">
+            <Box sx={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
+              <SettingsBrightnessIcon fontSize='small'/>System
+            </Box>
+          </MenuItem>
+        </Select>
+      </FormControl>
+    </>
+  )
+}
+
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme()
+  return (
+    <Button
+      onClick={() => {
+        setMode(mode === 'light' ? 'dark' : 'light')
+      }}
+    >
+      {mode === 'light' ? 'Turn dark' : 'Turn light'}
+    </Button>
+  )
+}
 
 function App() {
   return (
     <>
+      <ModeSelect />
+      <hr />
+      <ModeToggle/>
+      <hr/>
       <div>KennyCode</div>
 
       <Typography variant="body2" color= "text.secondary" >Test Typography </Typography>
--- src/theme.js
+++ src/theme.js
@@ -1,21 +1,20 @@
-import { createTheme } from '@mui/material/styles'
-import { red } from '@mui/material/colors'
+import { cyan, deepOrange, orange, teal } from '@mui/material/colors'
+import { experimental_extendTheme as extendTheme} from '@mui/material/styles'
 
 // Create a theme instance.
-const theme = createTheme({
-  palette: {
-    mode: 'light',
-    primary: {
-      main: '#F55536'
+const theme = extendTheme({
+  colorSchemes: {
+    light: {
+      palette: {
+        primary: teal,
+        secondary: deepOrange
+      }
     },
-    secondary: {
-      main: '#FF773D'
-    },
-    error: {
-      main: red.A400
-    },
-    text: {
-      secondary: red[500]
+    dark: {
+      palette: {
+        primary: cyan,
+        secondary: orange
+      }
     }
   }
 })
